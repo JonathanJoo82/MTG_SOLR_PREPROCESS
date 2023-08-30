e5a88b826ccfc3061f2d726e3fa10b0521924a7e
root <root@DESKTOP-CBR1UVD>
2023-08-30 00:52:05 -0500

    modify script

--- scripts/parseMTGJsonData.sh
+++ scripts/parseMTGJsonData.sh
@@ -2,12 +2,16 @@
 set -x
 
 RAW_DATA_DIR=/mnt/c/Users/jonat/Project/Data/MTG_data/rawData
-DATA_DONE_DIR=/mnt/c/Users/jonat/Project/Data/MTG_data/parsedJsonDataForSolr/done/
+DATA_DONE_DIR=/mnt/c/Users/jonat/Project/Data/MTG_data/parsedJsonDataForSolr/done
 JAR_TARGET_DIR=/mnt/c/Users/jonat/Programming/MTG_SOLR_PREPROCESS/target
 PROPERTY=../property/all_path.properties
 ALLPRINTDATA=/mnt/c/Users/jonat/Project/Data/MTG_data/rawData/AllPrintings.json
 PARSED_DATA_CURRENT=/mnt/c/Users/jonat/Project/Data/MTG_data/parsedJsonDataForSolr/current/
 BACKUP_RAW_DATA=/mnt/c/Users/jonat/Project/Data/MTG_data/backupRawData/
+S3_BUCKET=magic-the-gathering-s3
+PREFIX=inbound/solr
+numberRange="{1..10000} {10001..20000} {20001..30000} {30001..40000} {40001..50000} {50001..60000} {60001..70000} {80001..90000} {90001..100000}"
+count=1
 
 
 cd $RAW_DATA_DIR
@@ -30,6 +34,16 @@ gzip AllPrintings*
 mv AllPrintings* $BACKUP_RAW_DATA
 
 cd $PARSED_DATA_CURRENT
+
+
+
+for range in $numberRange
+do
+  tar -czf fullMTGdata.json_"$count"_"${timestamp}".tar.gz CardData"$range".json
+  rm CardData"$range".json
+  count=$((count + 1))
+done
+
 
 tar -czf fullMTGdata.json_1_10000_${timestamp}.tar.gz CardData{1..10000}.json
 rm CardData{1..10000}.json
@@ -54,3 +68,4 @@ rm CardData{90001..100000}.json
 
 mv *tar.gz $DATA_DONE_DIR
 
+aws s3 cp $DATA_DONE_DIR s3://$S3_BUCKET/$PREFIX --recursive
